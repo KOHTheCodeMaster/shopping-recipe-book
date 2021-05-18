c98b8b050150e7edfb6c79840638959a6ba7e230
kohthecodemaster <kohthecodemaster@gmail.com>
2021-05-18 16:08:59 +0530

    4th Commit - RecipePOJO & Recipe List Structure
    Signed-off-by: kohthecodemaster <kohthecodemaster@gmail.com>

--- Commit-Logs.md
+++ Commit-Logs.md
@@ -1,3 +1,8 @@
+# 4th Commit - RecipePOJO & Recipe List Structure
+------------------------------
+1. Designed the structure of the recipe list.
+2. Added RecipePOJO model.
+
 # 3rd Commit - Navbar
 ------------------------------
 1. Navbar with Bootstrap classes added.
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RecipePOJO} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipeList: RecipePOJO[] = [
+    new RecipePOJO(
+      'Black Burger',
+      'Kuro Burger is a product line of hamburger sold by the Japanese franchises of the Burger King fast food restaurant chain. The buns and the cheese of the hamburger are colored black with bamboo charcoal.',
+      'https://static-ssl.businessinsider.com/image/56057b299dd7cc11008bd1b2-784-588/bk%20halloween%20whopper%5B2%5D.jpg'
+    ),
+    new RecipePOJO(
+      'Black Burger',
+      'The buns and the cheese of the hamburger are colored black with bamboo charcoal.',
+      'https://static-ssl.businessinsider.com/image/56057b299dd7cc11008bd1b2-784-588/bk%20halloween%20whopper%5B2%5D.jpg'
+    )
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
